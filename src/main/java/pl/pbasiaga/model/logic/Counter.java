452b86cb700da0d2071e88aefe42c18ecb83d13b
PBasiaga <przemek.basiaga23@gmail.com>
2021-03-04 18:18:56 +0100

    implement counting letter occurrenves

--- src/main/java/pl/pbasiaga/model/logic/Counter.java
+++ src/main/java/pl/pbasiaga/model/logic/Counter.java
@@ -20,8 +20,10 @@ public class Counter {
 
     public TextInformation calculateTextInformation() {
         TextInformation textInformation = new TextInformation();
-        textInformation.setLongestWord(findLongestWord());
         textInformation.setWordCount(getWordCount());
+        textInformation.setLongestWord(findLongestWord());
+        textInformation.setLetterOccurrences(countLetterOccurrences());
+
 
         return textInformation;
     }
@@ -39,6 +41,54 @@ public class Counter {
     private int getWordCount() {
 
         return words.size();
+    }
+
+    private HashMap<Character, Integer> countLetterOccurrences() {
+        HashMap<Character, Integer> occurrences = initOccurrences();
+
+        for (String word : words) {
+            for (char letter: word.toCharArray()) {
+                if (Character.isLetter(letter)) {
+                    char upperCaseLetter = Character.toUpperCase(letter);
+                    Integer count = occurrences.get(upperCaseLetter);
+                    count++ ;
+                    occurrences.put(upperCaseLetter, count);
+                }
+            }
+        }
+        return occurrences;
+    }
+
+    private HashMap<Character, Integer> initOccurrences() {
+        HashMap<Character, Integer> occurrences = new HashMap<>();
+        occurrences.put('A', 0);
+        occurrences.put('B', 0);
+        occurrences.put('C', 0);
+        occurrences.put('D', 0);
+        occurrences.put('E', 0);
+        occurrences.put('F', 0);
+        occurrences.put('G', 0);
+        occurrences.put('H', 0);
+        occurrences.put('I', 0);
+        occurrences.put('J', 0);
+        occurrences.put('K', 0);
+        occurrences.put('L', 0);
+        occurrences.put('M', 0);
+        occurrences.put('N', 0);
+        occurrences.put('O', 0);
+        occurrences.put('P', 0);
+        occurrences.put('Q', 0);
+        occurrences.put('R', 0);
+        occurrences.put('S', 0);
+        occurrences.put('T', 0);
+        occurrences.put('U', 0);
+        occurrences.put('V', 0);
+        occurrences.put('W', 0);
+        occurrences.put('X', 0);
+        occurrences.put('Y', 0);
+        occurrences.put('Z', 0);
+
+        return occurrences;
     }
 
 
