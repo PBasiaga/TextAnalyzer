5592093249b65b516df400625655fd49d85c438c
PBasiaga <przemek.basiaga23@gmail.com>
2021-03-04 17:36:36 +0100

    implement fileReader in class Reader

--- src/main/java/pl/pbasiaga/model/logic/Reader.java
+++ src/main/java/pl/pbasiaga/model/logic/Reader.java
@@ -1,4 +1,36 @@
 package pl.pbasiaga.model.logic;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class Reader {
+
+    public List<String> getWordsFromFile(String filename) throws FileNotFoundException {
+
+        List<String> wordsList = new ArrayList<>();
+        String sentenceWithoutSpecialSigns;
+
+        Scanner scanner = new Scanner(new FileReader(filename));
+        scanner.useDelimiter("\n");
+        while (scanner.hasNextLine()) {
+
+            sentenceWithoutSpecialSigns = scanner.nextLine().replaceAll("[^a-zA-Z0-9]", " ");
+            sentenceWithoutSpecialSigns = sentenceWithoutSpecialSigns
+                    .strip()
+                    .replaceAll("\\s+", " ");
+
+            String[] words = sentenceWithoutSpecialSigns.split(" ");
+
+            for (String word : words) {
+                wordsList.add(word);
+            }
+        }
+
+        return wordsList;
+    }
+
 }
