8218393ed678eaa20697019925398c1cd4598dce
PBasiaga <przemek.basiaga23@gmail.com>
2021-03-04 18:47:32 +0100

    implement menu for choosing text information

--- src/main/java/pl/pbasiaga/controller/Controller.java
+++ src/main/java/pl/pbasiaga/controller/Controller.java
@@ -30,4 +30,33 @@ public class Controller {
         counter.setWords(words);
         textInformation = counter.calculateTextInformation();
     }
+
+    private void handleFileMenu() {
+        int menuValue = 1;
+        while (menuValue != 0) {
+            menuValue = view.showFileDetailsMenuAndGetSelectedPosition();
+            switch (menuValue) {
+                case 1:
+                    view.showNumberOfWordsInFile(textInformation.getWordCount());
+                    break;
+                case 2:
+                    view.showLongestWordInFile(textInformation.getLongestWord());
+                    break;
+                case 3:
+                    view.showAverageNumberOfLettersInWord(textInformation.getAverageNumberOfLettersInWord());
+                    break;
+                case 4:
+                    view.showLetterOccurrences(textInformation.getLetterOccurrences());
+                    break;
+                case 0:
+                    return;
+                default:
+                    view.showIncorrectNumberInformation();
+                    break;
+
+            }
+
+        }
+
+    }
 }
--- src/main/java/pl/pbasiaga/view/View.java
+++ src/main/java/pl/pbasiaga/view/View.java
@@ -1,5 +1,7 @@
 package pl.pbasiaga.view;
 
+import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class View {
@@ -10,5 +12,56 @@ public class View {
         String filename = scanner.nextLine();
 
         return filename;
+    }
+
+    public int showFileDetailsMenuAndGetSelectedPosition() {
+
+        int option;
+
+        try {
+            System.out.println("Enter a number: ");
+            System.out.println("[1] Show number of words in the file.");
+            System.out.println("[2] Show longest word in the file.");
+            System.out.println("[3] Show average number of letters in the file.");
+            System.out.println("[4] Show occurrences of letters in file.");
+            System.out.println("[0] Return");
+
+            Scanner scanner = new Scanner(System.in);
+            option = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            option = -1;
+        }
+        return option;
+    }
+
+    public void showNumberOfWordsInFile(int number) {
+        System.out.println("The number of words in your file is: " + number);
+        System.out.println();
+    }
+
+    public void showLongestWordInFile(String word) {
+        if (!word.equals("")) {
+            System.out.println("The longest word in your file is: " + word);
+        } else {
+            System.out.println("There are no words in this file.");
+        }
+        System.out.println();
+    }
+
+    public void showAverageNumberOfLettersInWord(double number) {
+        System.out.printf("Average number of letters in word is: %.2f \n", number);
+        System.out.println();
+    }
+
+    public void showLetterOccurrences(HashMap<Character, Integer> letterOccurrences) {
+        System.out.println("Number of letter occurrences: ");
+        System.out.println();
+        letterOccurrences.forEach((key, value) -> System.out.println(key + " : " + value));
+        System.out.println();
+    }
+
+    public void showIncorrectNumberInformation() {
+        System.out.println("Use only valid numbers.");
+        System.out.println();
     }
 }
