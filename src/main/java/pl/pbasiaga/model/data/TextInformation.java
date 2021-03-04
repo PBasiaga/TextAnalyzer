de198d3585a134d2626628d86cdfee27fac3cb5a
PBasiaga <przemek.basiaga23@gmail.com>
2021-03-04 17:48:15 +0100

    implement class containing TextInformation

--- src/main/java/pl/pbasiaga/model/data/TextInformation.java
+++ src/main/java/pl/pbasiaga/model/data/TextInformation.java
@@ -1,4 +1,49 @@
 package pl.pbasiaga.model.data;
 
+import java.util.HashMap;
+
 public class TextInformation {
+
+    private int wordCount;
+
+    private double averageNumberOfLettersInWord;
+
+    private String longestWord;
+
+    private HashMap<Character, Integer> letterOccurrences;
+
+
+    public int getWordCount() {
+        return wordCount;
+    }
+
+    public void setWordCount(int wordCount) {
+        this.wordCount = wordCount;
+    }
+
+    public double getAverageNumberOfLettersInWord() {
+        return averageNumberOfLettersInWord;
+    }
+
+    public void setAverageNumberOfLettersInWord(double averageNumberOfLettersInWord) {
+        this.averageNumberOfLettersInWord = averageNumberOfLettersInWord;
+    }
+
+    public String getLongestWord() {
+        return longestWord;
+    }
+
+    public void setLongestWord(String longestWord) {
+        this.longestWord = longestWord;
+    }
+
+    public HashMap<Character, Integer> getLetterOccurrences() {
+        return letterOccurrences;
+    }
+
+    public void setLetterOccurrences(HashMap<Character, Integer> letterOccurrences) {
+        this.letterOccurrences = letterOccurrences;
+    }
+
+
 }
